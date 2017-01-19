47f254fe699e7f963b52d504a1e6a46e8ce3f709
Yessy Permatasari <yessypermatasari08@gmail.com>
2017-01-18 23:47:23 -0800

    RecyclerView Grid

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/MainActivity.java
@@ -1,14 +1,10 @@
 package id.sch.smktelkom_mlg.learn.recyclerview2;
-
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -25,8 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
         mAdapter = new HotelAdapter(mlist);
         recyclerView.setAdapter(mAdapter);
@@ -42,18 +39,16 @@ public class MainActivity extends AppCompatActivity {
         Drawable[] arFoto = new Drawable[a.length()];
 
         for (int i = 0; i < arFoto.length; i++) {
-            BitmapDrawable bd = (BitmapDrawable) a.getDrawable(i);
-            RoundedBitmapDrawable rbd =
-                    RoundedBitmapDrawableFactory.create(getResources(), bd.getBitmap());
-            rbd.setCircular(true);
-            arFoto[i] = rbd;
+            arFoto[i] = a.getDrawable(i);
         }
 
+        a.recycle();
 
         for (int i = 0; i < arjudul.length; i++) {
             mlist.add(new Hotel(arjudul[i], arDeskripsi[i], arFoto[i]));
         }
         mAdapter.notifyDataSetChanged();
-
     }
+
+
 }
